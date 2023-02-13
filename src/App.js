158ea2908cb9cff60fdfaa9d87712033268a6558
musaddiqshaheer <musaddiqs784@gmail.com>
2023-02-13 11:09:28 +0530

    first push

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Bulb } from "./Components/Bulb";
+
+function App() {
+  return (
+    <div className="App">
+      <Bulb />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Euqulae.js
@@ -0,0 +1,1 @@
+//how to connect ui to mongodb?
